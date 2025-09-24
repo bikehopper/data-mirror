6360453abd5910eb22301b8b81fe38afd1999b2b
Arindam Bose <arindam.1993@gmail.com>
2025-09-23 20:01:57 -0700

    Use double-quotes to denote strong etag (#5)

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ app.get('/gtfs.zip', (req, res) => {
     return;
   }
 
-  res.setHeader('ETag', CHECKSUMS.gtfs);
+  res.setHeader('ETag', `"${CHECKSUMS.gtfs}"`);
   res.sendFile(path.join(DATA_PATH, 'gtfs.zip'));
 });
 
@@ -40,7 +40,7 @@ app.get('/osm.pbf', (req, res) => {
     res.sendStatus(404);
     return;
   }
-  res.setHeader('ETag', CHECKSUMS.osm);
+  res.setHeader('ETag', `"${CHECKSUMS.osm}"`);
   res.sendFile(path.join(DATA_PATH, 'osm.pbf'));
 });
 
