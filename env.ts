cc18c5b3efba64bb8742b3eebc0abd087a361534
Arindam Bose <arindam.1993@gmail.com>
2025-09-27 21:12:42 -0700

    add new files (#18)
    * add new files
    
    * fix handling of bad content-tength from github

--- env.ts
+++ env.ts
@@ -4,4 +4,9 @@ export const GTFS_URL: string = process.env.GTFS_URL ||
   `http://api.511.org/transit/datafeeds?api_key=${process.env.API_511_KEY}&operator_id=RG`;
 export const OSM_PBF_URL: string = process.env.OSM_PBF_URL || 
   'https://download.geofabrik.de/north-america/us/california/norcal-latest.osm.pbf';
+export const REGION_CONFIG_URL: string = process.env.REGION_CONFIG_URL ||
+  'https://raw.githubusercontent.com/bikehopper/manual-datafiles/refs/heads/main/region-config.json';
+export const ELEVATORS_CSV_URL: string = process.env.ELEVATORS_CSV_URL || 
+  'https://raw.githubusercontent.com/bikehopper/manual-datafiles/refs/heads/main/elevators.csv';
+
 export const REFRESH_KEY: string = process.env.REFRESH_KEY || '';
--- lib/fetch-files.ts
+++ lib/fetch-files.ts
@@ -1,13 +1,17 @@
-import { GTFS_URL, OSM_PBF_URL } from '../env.js';
+import { ELEVATORS_CSV_URL, GTFS_URL, OSM_PBF_URL, REGION_CONFIG_URL } from '../env.js';
 import path from 'node:path';
 import fs, { existsSync, mkdirSync, rmSync } from 'node:fs';
 import { calcChecksum } from './calc-checksum.js';
 import { fetchBlob } from './fetch-blob.js';
 import logger from './logger.js';
 
-export const CHECKSUMS: Record<'gtfs' | 'osm', string | null> = {
+export type SourceDataFileType = 'gtfs' | 'osm' | 'region_config' | 'elevators';
+
+export const CHECKSUMS: Record<SourceDataFileType, string | null> = {
   gtfs: null,
   osm: null,
+  region_config: null,
+  elevators: null,
 };
 
 export const DATA_PATH = path.join(process.cwd(), '/data');
@@ -21,11 +25,15 @@ export const updateFiles = async (): Promise<void> => {
   isUpdating = true;
   const gtfsZip = await fetchBlob(GTFS_URL, (value: string) => logger.info(value));
   const osmPbf = await fetchBlob(OSM_PBF_URL, (value: string) => logger.info(value));
+  const regionConfig = await fetchBlob(REGION_CONFIG_URL, (value: string) => logger.info(value), true);
+  const elevators = await fetchBlob(ELEVATORS_CSV_URL, (value: string) => logger.info(value), true);
 
   logger.info('Finished fetching data');
 
   CHECKSUMS.gtfs = calcChecksum(gtfsZip);
   CHECKSUMS.osm = calcChecksum(osmPbf);
+  CHECKSUMS.region_config = calcChecksum(regionConfig);
+  CHECKSUMS.elevators = calcChecksum(elevators);
 
   logger.info(JSON.stringify(CHECKSUMS, null, 2));
 
@@ -37,6 +45,8 @@ export const updateFiles = async (): Promise<void> => {
   mkdirSync(DATA_PATH);
   fs.writeFileSync(path.join(DATA_PATH, 'gtfs.zip'), gtfsZip);
   fs.writeFileSync(path.join(DATA_PATH, 'osm.pbf'), osmPbf);
+  fs.writeFileSync(path.join(DATA_PATH, 'region-config.json'), regionConfig);
+  fs.writeFileSync(path.join(DATA_PATH, 'elevators.csv'), elevators);
 
   isUpdating = false;
 }
