12d478a77e06cff6c4ee62c432ccc04e775438bb
Arindam Bose <arindam.1993@gmail.com>
2025-12-28 19:37:01 -0800

    address CR comments

--- lib/fetch-rt-pbfs.ts
+++ lib/fetch-rt-pbfs.ts
@@ -3,14 +3,12 @@ import { REALTIME_PBFS, type RealtimeSourceType } from '../rt-pbfs.js';
 import { ApiKeyPool } from './api-key-pool.js';
 import { fetchBlob } from './fetch-blob.js';
 import logger from './logger.js';
-import ControlablePromise from './controlable-promise.js';
 
 const pbfCache: Map<RealtimeSourceType, Buffer> = new Map();
 let lastUpdateTime: number = -1;
 
-// Stash an already-resolved promise to start
 // This MUST stay a singleton instance, this allows multiple calls on `getRtPbfs` to wait on the same update
-let updatePromise: ControlablePromise<void> = (new ControlablePromise<void>()).resolve();
+let updatePromise: Promise<void> | null = null;
 
 let pool: ApiKeyPool | null = null;
 try {
@@ -18,6 +16,18 @@ try {
 } catch (e) {
   logger.info('Could not initialize API key pool, gtfs-rt will not be available');
 }
+
+type PbfPromise = Promise<{
+  pbfName: RealtimeSourceType,
+  buffer: Buffer,
+}>;
+
+const fetchPbf = async ( pbfName: RealtimeSourceType,  urlTemplate: string, apiKeyPool: ApiKeyPool ): PbfPromise => {
+  const apiKey = apiKeyPool.getKey();
+  const url = urlTemplate.replace('{API_KEY}', apiKey);
+  const buffer = await fetchBlob(url, (value: string) => logger.info(value));
+  return {pbfName, buffer};
+};
 
 /**
  * Fetches new-rt pbfs from 511 Endpoints
@@ -31,57 +41,56 @@ const updateRealtimePbfs = async (): Promise<void> => {
     return;
   }
 
-  if (updatePromise.inFlight) {
+  if (updatePromise != null) {
     logger.info('updateRealtimePbfs early return because a update is already in fight');
-    return;
+    return updatePromise;
   }
 
   // Check if data needs Update
-  const timeDiff = Math.abs(Date.now() - lastUpdateTime);
-  const dataIsStale = timeDiff > GTFS_RT_UPDATE_INTERVAL_MS;
+  const timeDiff = Date.now() - lastUpdateTime;
+  const needsUpdate = timeDiff > GTFS_RT_UPDATE_INTERVAL_MS;
 
-  logger.info(`updateRealtimePbfs: dataIsStale: ${dataIsStale}, lastUpdateTime: ${lastUpdateTime}, timeDiff: ${timeDiff}`);
-
-  const noData = pbfCache.size === 0;
-  const prevUpdateFailed = updatePromise.rejected;
-
-  const needsUpdate = dataIsStale || noData || prevUpdateFailed;
+  logger.info(`updateRealtimePbfs: needsUpdate: ${needsUpdate}, lastUpdateTime: ${lastUpdateTime}, timeDiff: ${timeDiff}`);
 
   if (!needsUpdate) {
     return;
   }
 
-  updatePromise = new ControlablePromise<void>();
+  let resolver = () => {};
+  let rejecter = (reason?: any) => {};
+  updatePromise = new Promise((resolve, reject) => {
+    resolver = resolve;
+    rejecter = reject;
+  });
 
   try {
-    const newCache: Map<RealtimeSourceType, Buffer>= new Map();
+    const promises: PbfPromise[] =[];
     for (const [pbfName, urlTemplate] of Object.entries(REALTIME_PBFS)){
-      const apiKey = pool.getKey();
-      const url = urlTemplate.replace('{API_KEY}', apiKey);
-      console.log(url);
-      const buff = await fetchBlob(url, (value: string) => logger.info(value));
-      newCache.set(pbfName as RealtimeSourceType, buff);
+      promises.push(fetchPbf(pbfName as RealtimeSourceType, urlTemplate, pool));
     }
-  
+    
+    const buffers = await Promise.all(promises);
     // Everything is async is done, so now atomically update pbfCache
     // This ensures that no read happens while writes are potentially inflight on the Map
     pbfCache.clear();
-    for (const [pbfName, buff] of newCache.entries()){
-      pbfCache.set(pbfName, buff);
+    for (const {pbfName, buffer} of buffers){
+      pbfCache.set(pbfName, buffer);
     }
 
-
-    updatePromise.resolve();
     lastUpdateTime = Date.now();
+    resolver();
   } catch (e) {
-    updatePromise.reject(e);
+    rejecter(e);
   } 
+
+  const updatePromiseStash = updatePromise;
+  // Set the global ref to null do denote that no updates are in flight
+  updatePromise = null;
+  return updatePromiseStash;
 };
 
 export const getRealtimePbf = async (name: RealtimeSourceType): Promise<Buffer | undefined> => {
-  updateRealtimePbfs();
-  // ALL requests await on the singleton instance of the updatePromise
-  await updatePromise.nativePromise();
+  await updateRealtimePbfs();
 
   return pbfCache.get(name);
 };
@@ -89,5 +98,5 @@ export const getRealtimePbf = async (name: RealtimeSourceType): Promise<Buffer |
 export const resetInternaPbfCacheState = () => {
   lastUpdateTime = -1;
   pbfCache.clear();
-  updatePromise = (new ControlablePromise<void>()).resolve();
+  updatePromise = null;
 };
